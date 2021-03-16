6ca4579f6aa239cc7eb5522c7fab688245b55b27
qiuxk <qxk1101@163.com>
2021-03-16 16:09:49 +0800

    代码格式化

--- src/main/java/com/qiuxk/more_thread/base/controller/KafkaSendMessageController.java
+++ src/main/java/com/qiuxk/more_thread/base/controller/KafkaSendMessageController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class KafkaSendMessageController {
-    @Autowired
+    @Autowired(required = false)
     private KafkaTemplate<String, Message> kafkaTemplate;
 
     @GetMapping("send/{message}")
